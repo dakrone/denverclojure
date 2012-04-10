150d63661505f50ac804fcc454cdd8d5e231d34f
Michael Hunsinger <mike.hunsinger@gmail.com>
2012-04-10 12:02:04 -0600

    Added a page about getting Clojure started with emacs and leiningen

--- src/denverclojure/core.clj
+++ src/denverclojure/core.clj
@@ -16,13 +16,67 @@
 (defpage "/html" []
   (html [:div#foo.bar "Hello World"]))
 
+(defpage "/startingclojure" []
+  (html [:h1 "Starting Clojure!"]
+        [:p "So you've decided to dabble in Clojure?
+             Before you get started there are a few things you'll need
+             before you get hacking."]
+        [:h3 "Installing Leiningen:"]
+        [:ol "Leiningen makes working with Clojure bareable. Most people think
+             its the bee's knees and we'll be using it to make and use projects
+             at the Denver Clojure Meetup. You can find the link to technomancy's
+             leiningen GitHub below. Install instructions are also located
+             on the page."]
+        [:ol (link-to "https://github.com/technomancy/leiningen" "Leiningen")]
+        [:h3 "Installing Emacs and Marmalade:"]
+        [:ol "You can use any text editor you wish to edit your clojure files,
+             but it would probably be we'll be using Emacs and several of it's
+             extensions at the meetups. If you aren't interested in installing
+             or learning emacs, then the rest of this may not be useful for you
+             as it has to deal with setting up emacs to work with Clojure."]
+        [:ol (link-to "http://emacsformacosx.com/" "Emacs for OSX")
+         " You'll probably be safe installing v24."]
+        [:ol (link-to "https://code.google.com/p/emacs-for-windows/downloads/list"
+                      "Emacs for Windows")]
+        [:ol "Got it installed? Great, now we're going to install a file from "
+         (link-to "http://marmalade-repo.org/" "Marmalade")
+         " this will allow you to easily download the extensions we need
+         (as well as others) to work with Clojure. Open up a blank buffer
+         in emacs by typing \"emacs\" in your terminal and then copy and
+         paste the following into the blank buffer."]
+        [:ol
+         [:pre "(progn "
+          [:ol "(switch-to-buffer"]
+          [:ol "(url-retrieve-synchronously \"http://repo.or.cz/w/emacs.git/blob_plain/1a0a666f941c99882093d7bd08ced15033bc3f0c:/lisp/emacs-lisp/package.el\"))"]
+          [:br "(package-install-from-buffer  (package-buffer-info) 'single))"]]]
+        [:ol "Now type \"M-x eval-buffer\" (when dealing with emacs, M and C refer to
+              Meta and Control, respectively). This should write the file from
+              Marmalade and then install it into your emacs file. In emacs type in
+              \"M-x package-list-packages\" and find clojure-mode. Place an \"i\"
+              next to the package and hit the \"x\" key and accept the installation. 
+              You're almost ready to start hacking!"]
+        [:ol [:center [:h4 "At this point, I would say if you're not familiar with some
+              of the basic commands with leiningen (especially the \"new\" command) or
+              emacs, it would be worth your time to play around and read the tutorials
+              on how to use these. From here on out, we're going to assume you know
+              how to create a new project."]]]
+        [:h3 "Gettin' Swanky:"]
+        [:ol "Open up your core.clj and type in \"M-x clojure-jack-in\".
+              Assuming everything went right, this should open up another buffer
+              with a REPL. Compile your file using \"C-c C-k\" then change the
+              namespace (so the REPL has access to your functions) by typing in
+              \"C-c M-p\" and accepting your namespace. Now you're ready!
+              Of course you'll need to make some functions in your core.clj file
+              in order to have any fun and that may be beyond the scope of this
+              tutorial but you're now set up to effeciently process Clojure
+              functions in emacs."]))
+
 (defpartial user [n]
   [:p [:h2 (str "Hello " n)]])
 
 (defpartial users [names]
   (for [name names]
     (user name)))
-
 
 (def members [{:name "Lee"
                :github "http://github.com/dakrone"}
@@ -33,15 +87,26 @@
               {:name "Gregory"
                :github "http://github.com/gregoryg"}
               {:name "Bart"
-               :github "http://github.com/kasterma"}])
+               :github "http://github.com/kasterma"}
+              {:name "Michael"
+               :github "http://www.github.com/mikehunsinger"}])
 
-(def links [{:url "http://overtone.github.com/" :desc "A music creation using Clojure" :link-text "Overtone"}
-            {:url "http://webnoir.org/" :desc "Noir, a web framework"}
-            {:url "https://github.com/cgrand/enlive" :desc "A templating system for Clojure with CSS-like selectors" :link-text "Enlive"}
+(def links [{:url "http://overtone.github.com/"
+             :desc "A music creation using Clojure"
+             :link-text "Overtone"}
+            {:url "http://webnoir.org/"
+             :desc "Noir, a web framework"
+             :link-text "Noir"}
+            {:url "https://github.com/cgrand/enlive"
+             :desc "A templating system for Clojure with CSS-like selectors"
+             :link-text "Enlive"}])
 
-            ])
-
-
+(def clojuregoodness [{:url "startingclojure"
+                       :desc "Startup guide for installing and getting an
+                              environment set up to run clojure"
+                       :link-text "Starting Clojure"}
+                      {:url "/html"
+                       :link-text "foo"}])
 
 (defpartial make-user [member]
   [:li (str (:name member) " - ") (link-to (:github member) "github")])
@@ -62,6 +127,8 @@
    [:ul#members (map make-user members)]
    [:h2 "Interesting links"]
    [:ul#links (map make-link links)]
+   [:h2 "Clojure Goodness"]
+   [:ul#clojuregoodness (map make-link clojuregoodness)]
     ))
 
 (defpage "/users.json" []
