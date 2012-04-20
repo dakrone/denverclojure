ecdd6e2dc1ee5c6c171fa4484b843482e7104025
Lee Hinman <lee@writequit.org>
2012-04-19 19:59:45 -0600

    Add ability to show upcoming events

--- project.clj
+++ project.clj
@@ -2,6 +2,7 @@
   :url "http://denverclojure.herokuapp.com/"
   :description "The Denver Clojure website"
   :dependencies [[org.clojure/clojure "1.4.0"]
-                 [noir "1.3.0-beta2"]]
+                 [noir "1.3.0-beta2"]
+                 [clj-http "0.3.6"]]
   :dev-dependencies [[lein-swank "1.4.3"]]
   :main denverclojure.core)
--- src/denverclojure/core.clj
+++ src/denverclojure/core.clj
@@ -1,11 +1,15 @@
 (ns denverclojure.core
-  (:use [noir.core]
-        [hiccup.core]
-        [hiccup.page]
-        [hiccup.form]
-        [hiccup.element]
-        [cheshire.core])
-  (:require [noir.server :as server]))
+  (:require [clojure.java.io :refer [resource]]
+            [noir.core :refer :all]
+            [hiccup.core :refer :all]
+            [hiccup.page :refer :all]
+            [hiccup.form :refer :all]
+            [hiccup.element :refer :all]
+            [cheshire.core :refer :all]
+            [noir.server :as server]
+            [clj-http.client :as http]))
+
+(def api-key (.trim (slurp (resource "key"))))
 
 (defpage "/user/:id" {:keys [id]}
   (str "Hello " id))
@@ -23,6 +27,53 @@
   (for [name names]
     (user name)))
 
+;; {:group
+;;  {:id 1352976,
+;;   :group_lat 37.380001068115234,
+;;   :name "The Bay Area Clojure User Group",
+;;   :group_lon -122.08000183105469,
+;;   :join_mode "open",
+;;   :urlname "The-Bay-Area-Clojure-User-Group",
+;;   :who "Clojure programmers"},
+;;  :status "upcoming",
+;;  :maybe_rsvp_count 0,
+;;  :name "The Bay Area Clojure User Group Late/MV Monthly Meetup",
+;;  :utc_offset -25200000,
+;;  :yes_rsvp_count 29,
+;;  :waitlist_count 0,
+;;  :created 1334077505000,
+;;  :updated 1334599365000,
+;;  :time 1334883600000,
+;;  :event_url
+;;  "http://www.meetup.com/The-Bay-Area-Clojure-User-Group/events/59990302/",
+;;  :id "dshxscyqgbzb",
+;;  :description
+;;  "<p><strong>Julio: Exploring data with Clojure and Incanter: �A Hands On Introduction</strong></p>\n<p>�</p>\n<p>Incanter is a Clojure-based, R-like platform for statistical computing�and graphics. In this meeting we'll have an overview and short hands�on intro to Incanter. Then we will open it up to exploration,�discussion and experimentation so bring your laptops and data sets.</p>\n<p>�</p>\n<p>More information on Incanter can be found at <a href=\"http://incanter.org/\">http://incanter.org/</a></p>\n\n"}
+
+(defn format-event [event]
+  (select-keys event [:group :time :name :event_url]))
+
+(defn get-events
+  "Gets the events from Meetup.com"
+  []
+  ;; [{:name "Website Hackery" :description "Do some things."
+  ;;   :date (java.util.Date.)}
+  ;;  {:name "History of Lisp" :description "Do some other things."
+  ;;   :date (java.util.Date. (long 0))}]
+
+  (let [results (get-in (http/get "http://api.meetup.com/2/events"
+                                  {:query-params {:group_id "1674002"
+                                                  :key api-key}
+                                   :as :json})
+                        [:body :results])]
+    (map format-event results)))
+
+(defpartial event [e]
+  [:li [:h3 (str (-> e :group :name))] (:name e)])
+
+(defpage "/events" []
+  (html "Events:"
+        [:ul (map event (get-events))]))
 
 (def members [{:name "Lee"
                :github "http://github.com/dakrone"}
@@ -62,7 +113,7 @@
    [:ul#members (map make-user members)]
    [:h2 "Interesting links"]
    [:ul#links (map make-link links)]
-    ))
+   ))
 
 (defpage "/users.json" []
   (encode members))
