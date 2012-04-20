bdbed0e45ff899063f3f8f7d5b45aae03a356e1e
Lee Hinman <lee@writequit.org>
2012-04-19 19:13:24 -0600

    bump clojure to 1.4

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 (defproject denverclojure "1.0.0-SNAPSHOT"
   :url "http://denverclojure.herokuapp.com/"
   :description "The Denver Clojure website"
-  :dependencies [[org.clojure/clojure "1.3.0"]
+  :dependencies [[org.clojure/clojure "1.4.0"]
                  [noir "1.3.0-beta2"]]
   :dev-dependencies [[lein-swank "1.4.3"]]
   :main denverclojure.core)
